24a6c0258be785aaccee4b440ff7615f6afd6447
LeonHe <leonhe86@gmail.com>
2015-11-19 23:22:19 +0800

    server recive message pack

--- nodejs/FileMonitorClient.js
+++ nodejs/FileMonitorClient.js
@@ -14,6 +14,8 @@ var fileWriteStream = fs.createWriteStream('./'+filename,{
   mode: 0777
 });
 
+var socket = null;
+
 //TODO fs.watch检测改变的文件
 //TODO 初始化上传文件列表文件到客户端
 //TODO 匹配文件列表中文件是否有改动然后进行更新文件
@@ -67,35 +69,20 @@ function foreachDir(path,dir)
 foreachDir(root_path+"/src","src")
 foreachDir(root_path+"/res","res")
 
-// fileWriteStream.close()
 
-
-// 创建一个TCP服务器实例，调用listen函数开始监听指定端口
-// 传入net.createServer()的回调函数将作为”connection“事件的处理函数
-// 在每一个“connection”事件中，该回调函数接收到的socket对象是唯一的
-net.createServer(function(sock) {
-    // 我们获得一个连接 - 该连接自动关联一个socket对象
-    console.log('CONNECTED: ' +
-        sock.remoteAddress + ':' + sock.remotePort);
-
-
-    // 为这个socket实例添加一个"data"事件处理函数
-    sock.on('data', function(data) {
-        console.log('DATA ' + sock.remoteAddress + ': ' + data);
-        // 回发该数据，客户端将收到来自服务端的数据
-        //发送同步文件列表
-
-        var command=data[4]
-        console.log(command)
-         var buf=new Buffer(filename.length)
+var service ={}
+service["2000"] = function(data)
+{
+      console.log(data);
+        var buf=new Buffer(filename.length)
         buf.write(filename,0)
         console.log(buf.toString())
-        sendData(sock,1000,buf);
+        sendData(socket,1000,buf);
 
         fs.readFile("./"+filename, function (error, fileData) {
           if(error) throw error;
 
-          sendData(sock,1001,fileData);
+          sendData(socket,1001,fileData);
           // var file_list=String(fileData).split("\n")
           // // console.log(file_list)
           // file_list.forEach(function(filePath){
@@ -108,6 +95,36 @@ net.createServer(function(sock) {
 
           // })
         });
+}
+
+
+// fileWriteStream.close()
+
+
+// 创建一个TCP服务器实例，调用listen函数开始监听指定端口
+// 传入net.createServer()的回调函数将作为”connection“事件的处理函数
+// 在每一个“connection”事件中，该回调函数接收到的socket对象是唯一的
+net.createServer(function(sock) {
+    // 我们获得一个连接 - 该连接自动关联一个socket对象
+    console.log('CONNECTED: ' +
+        sock.remoteAddress + ':' + sock.remotePort);
+    socket = sock;
+
+    // 为这个socket实例添加一个"data"事件处理函数
+    sock.on('data', function(data) {
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // 回发该数据，客户端将收到来自服务端的数据
+        //发送同步文件列表
+        var reciv_data = data.toString()
+        var command=reciv_data.substr(0,4)
+        var data = reciv_data.substr(4,reciv_data.length)
+        var fun = service[command];
+        if(fun){
+            fun(data);
+        }
+        
+        // console.log("command:"+command+"   data:"+data)
+         
 });
 
     // 为这个socket实例添加一个"close"事件处理函数
