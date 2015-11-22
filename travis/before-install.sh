c0bc3690d6844a1e7580a38ffd01c8d62ef415bf
Yuanfei He <leonhe86@gmail.com>
2015-11-22 13:18:20 +0800

    fix:before-install script error

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -1,3 +1,2 @@
 #!/usr/bin
-
-curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>n
+curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
