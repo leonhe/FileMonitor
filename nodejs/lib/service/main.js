ef63c85bf82fb7490c68edf762602f16428f54ef
Yuanfei He <leonhe86@gmail.com>
2015-11-29 22:44:10 +0800

    add lib directory and rename FileMontior to index

--- nodejs/lib/service/main.js
+++ nodejs/lib/service/main.js
@@ -1,8 +1,17 @@
 /**
  * Created by yuanfei on 15/11/29.
  */
+/**
+ * Created by yuanfei on 15/11/29.
+ */
 var socket = null;
 exports.setSocket=function(value){
     socket = value;
     console.log("set socket");
+};
+
+exports.create = function (name) {
+    return {
+        name: name
+    };
 };
